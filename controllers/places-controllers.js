abb379cacf3a97e00b5e45c5f47adcea550ddc01
Juan <jcasalsbadolato@gmail.com>
2021-12-23 11:27:36 +0100

    Add controllers and refactor

---
+++ controllers/places-controllers.js
@@ -0,0 +1,58 @@
+const HttpError = require('../models/http-error');
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Churreria Paco",
+    description: "The best churros ever.",
+    location: {
+      lat: 39.573176,
+      lng: 2.634656,
+    },
+    address: "yes",
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Churreria Pepe",
+    description: "The not so best churros ever.",
+    location: {
+      lat: 39.573126,
+      lng: 2.634517,
+    },
+    address: "Close to yes",
+    creator: "u2",
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+  console.log("GET Request in places");
+
+  if (!place) {
+    throw new HttpError("Could not find place", 404);
+  }
+
+  res.json({ place: place });
+};
+
+const getPlacesByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+
+  console.log("users");
+
+  if (!place) {
+    return next(new HttpError("Could not find user", 404));
+  }
+
+  res.json({ place: place });
+}; 
+
+exports.getPlaceById = getPlaceById;
+exports.getPlacesByUserId = getPlacesByUserId;
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,61 +1,11 @@
 const express = require("express");
 
-const HttpError = require('../models/http-error');
+const placesControllers = require('../controllers/places-controllers');
 
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Churreria Paco",
-    description: "The best churros ever.",
-    location: {
-      lat: 39.573176,
-      lng: 2.634656,
-    },
-    address: "yes",
-    creator: "u1",
-  },
-  {
-    id: "p2",
-    title: "Churreria Pepe",
-    description: "The not so best churros ever.",
-    location: {
-      lat: 39.573126,
-      lng: 2.634517,
-    },
-    address: "Close to yes",
-    creator: "u2",
-  },
-];
+router.get("/:pid", placesControllers.getPlaceById);
 
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((p) => {
-    return p.id === placeId;
-  });
-  console.log("GET Request in places");
-
-  if (!place) {
-      throw new HttpError("Could not find place", 404);
-  }
-
-  res.json({ place: place });
-});
-
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((p) => {
-    return p.creator === userId;
-  });
-
-  console.log("users");
-
-  if (!place) {
-    return next(new HttpError("Could not find user", 404));
-  }
-
-  res.json({ place: place });
-});
+router.get("/user/:uid", placesControllers.getPlacesByUserId );
 
 module.exports = router;
