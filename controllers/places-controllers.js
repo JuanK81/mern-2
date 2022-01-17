01d33ca31f570ee33178f7018c127fc413467699
JuanK81 <jcasalsbadolato@gmail.com>
2022-01-17 13:22:28 +0100

    add updatepPlace and deletePLace controllers

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,6 +1,8 @@
-const HttpError = require('../models/http-error');
+const nodeid = require("node-id");
 
-const DUMMY_PLACES = [
+const HttpError = require("../models/http-error");
+
+let DUMMY_PLACES = [
   {
     id: "p1",
     title: "Churreria Paco",
@@ -33,7 +35,7 @@ const getPlaceById = (req, res, next) => {
   console.log("GET Request in places");
 
   if (!place) {
-    throw new HttpError("Could not find place", 404);
+    throw new HttpError("Could not find a place for the provided id.", 404);
   }
 
   res.json({ place: place });
@@ -41,6 +43,7 @@ const getPlaceById = (req, res, next) => {
 
 const getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid;
+
   const place = DUMMY_PLACES.find((p) => {
     return p.creator === userId;
   });
@@ -48,29 +51,66 @@ const getPlacesByUserId = (req, res, next) => {
   console.log("users");
 
   if (!place) {
-    return next(new HttpError("Could not find user", 404));
+    return next(
+      new HttpError("Could not find a place for the provided user id.", 404)
+    );
   }
 
   res.json({ place: place });
-}; 
+};
 
 const createPlace = (req, res, next) => {
-    const { title, description, coordinates, address, creator } = req.body
-    // const { id, title, description, location, address, creator } = req.body;
+  // const uuid = () => {
+  //   return (
+  //     "p" + Math.floor(Math.random() * 1000) + Math.floor(Math.random() * 1000)
+  //   );
 
-    const createdPlace = {
-        title,
-        description,
-        location: coordinates,
-        address,
-        creator,
-    };
+  // }
 
-    DUMMY_PLACES.push(createPlace); //ushift(createPlace) to add te new element to the beginning of the list
+  const { title, description, coordinates, address, creator } = req.body;
+  // const { id, title, description, location, address, creator } = req.body;
 
-    res.status(201).json({place: createdPlace})
+  
+  const createdPlace = {
+    id: nodeid(),
+    title, //title, short for title: title....
+    description,
+    location: coordinates,
+    // coordinates: location,
+    address,
+    creator,
+  };
+  
+  DUMMY_PLACES.push(createPlace); //ushift(createPlace) to add te new element to the beginning of the list
+  
+  res.status(201).json({ place: createdPlace });
+  console.log(createdPlace);
+};
+
+const updatePlace = (req, res, next) => {
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
+  
+  const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) };
+  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+  
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+  
+    res.status(200).json({ place: updatedPlace })
+
+};
+
+const deletePlace = (req, res, next) => {
+  const placeId = req.params.pid;
+  DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
+
+  res.status(200).json({message: "Place deleted."})
 };
 
 exports.getPlaceById = getPlaceById;
 exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
