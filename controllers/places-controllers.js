c7ba2fed276fc78d8761531ea84155e7daccffc7
JuanK81 <jcasalsbadolato@gmail.com>
2022-01-18 09:48:41 +0100

    add validation with express-validator

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,4 +1,5 @@
 const nodeid = require("node-id");
+const { validationResult } = require('express-validator')
 
 const HttpError = require("../models/http-error");
 
@@ -66,6 +67,11 @@ const createPlace = (req, res, next) => {
   //   );
 
   // }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    throw new HttpError('Invalid inputs passed', 422);
+  } 
 
   const { title, description, coordinates, address, creator } = req.body;
   // const { id, title, description, location, address, creator } = req.body;
@@ -88,6 +94,12 @@ const createPlace = (req, res, next) => {
 };
 
 const updatePlace = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    throw new HttpError("Invalid inputs passed", 422);
+  } 
+
   const { title, description } = req.body;
   const placeId = req.params.pid;
   
@@ -104,6 +116,11 @@ const updatePlace = (req, res, next) => {
 
 const deletePlace = (req, res, next) => {
   const placeId = req.params.pid;
+
+  if (!DUMMY_PLACES.find(p => p.id === placeId)) {
+    throw new HttpError('Could notfind a place for that id.', 404);
+  }
+
   DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
 
   res.status(200).json({message: "Place deleted."})
--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,60 +1,73 @@
 const nodeid = require("node-id");
+const { validationResult } = require('express-validator');
 
-const HttpError = require('../models/http-error')
+const HttpError = require("../models/http-error");
 
 const DUMMY_USERS = [
   {
     id: "u1",
     name: "Paco",
     email: "something@something.something",
-    password: "password"
+    password: "password",
   },
   {
     id: "u2",
     name: "Gallo",
     email: "pocpoc@something.something",
-    password: "password"
+    password: "password",
   },
 ];
 
 const getUsers = (req, res, next) => {
-  res.status(200).json({ users: DUMMY_USERS })
+  res.status(200).json({ users: DUMMY_USERS });
 };
 
 const signup = (req, res, next) => {
-
-  console.log('Haz algo!!');
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    throw new HttpError("Invalid inputs passed", 422);
+  } 
 
   const { name, email, password } = req.body;
 
-  console.log('Signup', name, email, password);
+  const hasUser = DUMMY_USERS.find((u) => u.email === email);
+  if (hasUser) {
+    throw new HttpError(
+      "Could not create user, cemail allready exists.",
+      422
+    );
+  }
+
+  console.log("Signup", name, email, password);
 
   const createdUser = {
     id: nodeid(),
     name,
     email,
-    password
+    password,
   };
 
   console.log(createdUser);
 
   DUMMY_USERS.push(createdUser);
 
-  res.status(201).json({user: createdUser})
+  res.status(201).json({ user: createdUser });
 };
 
 const login = (req, res, next) => {
   const { email, password } = req.body;
 
-  const identifiedUser = DUMMY_USERS.find(u => u.email === u.email);
-  if (!identifiedUser || !identifiedUser.password === password) {
+  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
+  console.log(identifiedUser.password, password);
+  if (!identifiedUser || identifiedUser.password !== password) {
     throw new HttpError(
       "Could not identify user, credentials seem to be wrong.",
       401
     );
   }
 
-  res.status(200).json({message: 'logged in!'})
+  res.status(200).json({ message: "logged in!" });
 };
 
 exports.getUsers = getUsers;
