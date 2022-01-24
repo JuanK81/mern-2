7b87740887720c352a2bc6199145585eed5cad96
JuanK81 <jcasalsbadolato@gmail.com>
2022-01-24 18:03:32 +0100

    add route protection tokens

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -77,7 +77,7 @@ const signup = async (req, res, next) => {
   try {
     token = jwt.sign(
       { userId: createdUser.id, email: createdUser.email },
-      'secret_private:key_to_be_chamged',
+      'secret_private:key_to_be_changed',
       { expiresIn: '1h' }
     );
   } catch (err) {
@@ -128,7 +128,7 @@ const login = async (req, res, next) => {
   try {
     token = jwt.sign(
       { userId: existingUser.id, email: existingUser.email },
-      'secret_private:key_to_be_chamged',
+      'secret_private:key_to_be_changed',
       { expiresIn: '1h' }
     );
   } catch (err) {
---
+++ middleware/check-auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const HttpError = require('../models/http-error');
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1]; //authorizaton: 'Bearer TOKEN'
+    if (!token) {
+      throw new Error('Authentication failed.');
+    }
+    const decodedToken = jwt.verify(token, 'secret_private:key_to_be_changed');
+    req.userData = { userId: decodedToken.userId };
+    next();
+  } catch (err) {
+    const error = new HttpError('Authentication failed.', 401);
+    return next(error);
+  }
+};
