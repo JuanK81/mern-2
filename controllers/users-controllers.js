ccd37826e47eddb01833edb21529312378d6f726
JuanK81 <jcasalsbadolato@gmail.com>
2022-01-24 17:45:40 +0100

    add hashed passwords

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,25 +1,27 @@
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const HttpError = require('../models/http-error');
 const User = require('../models/user');
 
-
 //GET USERS
 
 const getUsers = async (req, res, next) => {
-  
   let users;
 
   try {
     users = await User.find({}, '-password');
   } catch (err) {
-    const error = new HttpError('Fetching users failed. Please try again.', 500);
+    const error = new HttpError(
+      'Fetching users failed. Please try again.',
+      500
+    );
     return next(error);
   }
 
-  res.json({ users: users.map(user => user.toObject( { getters: true } ) ) })
+  res.json({ users: users.map((user) => user.toObject({ getters: true })) });
 };
-
 
 //SIGNUP
 
@@ -37,18 +39,21 @@ const signup = async (req, res, next) => {
   try {
     existingUser = await User.findOne({ email: email });
   } catch (err) {
-    const error = new HttpError(
-      'Signing up failed. Please try again.', 
-      500
-      );
+    const error = new HttpError('Signing up failed. Please try again.', 500);
     return next(error);
   }
 
   if (existingUser) {
-    const error = new HttpError(
-      'User exists already. Please login.',
-      422
-    );
+    const error = new HttpError('User exists already. Please login.', 422);
+    return next(error);
+  }
+
+  let hashedPassword;
+
+  try {
+    hashedPassword = await bcrypt.hash(password, 12);
+  } catch (err) {
+    const error = new Error('Could not create user.Please try again.', 500);
     return next(error);
   }
 
@@ -57,23 +62,33 @@ const signup = async (req, res, next) => {
     email,
     image: req.file.path,
     // image: req.file.path.replace("\\", "/"),
-    password,
-    places: []
+    password: hashedPassword,
+    places: [],
   });
 
   try {
     await createdUser.save();
   } catch (err) {
-    const error = new HttpError(
-      'Signing up failed. Please try again.',
-      500
-    );
+    const error = new HttpError('Signing up failed. Please try again.', 500);
     return next(error);
   }
 
-  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
-};
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: createdUser.id, email: createdUser.email },
+      'secret_private:key_to_be_chamged',
+      { expiresIn: '1h' }
+    );
+  } catch (err) {
+    const error = new HttpError('Signing up failed. Please try again.', 500);
+    return next(error);
+  }
 
+  res
+    .status(201)
+    .json({ userId: createdUser.id, email: createdUser.email, token: token });
+};
 
 //LOGIN
 
@@ -88,12 +103,44 @@ const login = async (req, res, next) => {
     return next(error);
   }
 
-  if (!existingUser || existingUser.password !== password) {
+  if (!existingUser) {
     const error = new HttpError('Invalid credentials. Please try again.', 401);
     return next(error);
   }
 
-  res.status(200).json({ message: 'logged in!', user: existingUser.toObject({ getters: true}) });
+  let isValidpassword = false;
+  try {
+    isValidpassword = await bcrypt.compare(password, existingUser.password);
+  } catch (err) {
+    const error = new Error(
+      'Could not log you in. Please check your credentials and try again',
+      500
+    );
+    return next(error);
+  }
+
+  if (!isValidpassword) {
+    const error = new HttpError('Invalid credentials. Please try again.', 401);
+    return next(error);
+  }
+
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: existingUser.id, email: existingUser.email },
+      'secret_private:key_to_be_chamged',
+      { expiresIn: '1h' }
+    );
+  } catch (err) {
+    const error = new HttpError('Logging in failed. Please try again.', 500);
+    return next(error);
+  }
+
+  res.status(200).json({
+    userId: existingUser.id,
+    email: existingUser.email,
+    token: token
+  });
 };
 
 exports.getUsers = getUsers;
